947b2dbfddf56e8dbbafe0cae7c4167d3f3fa91f
markcoma <dex.go@yandex.ru>
2022-04-12 21:47:16 +0300

    ContactBooks page

--- client/src/screens/ContactsBook/ContactsBook.tsx
+++ client/src/screens/ContactsBook/ContactsBook.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { сontactsAPI } from "../../api/api";
+import { ContactCard } from "../../components/ContactCard/ContactCard";
+import { CreateContact } from "../../components/CreateContact/CreateContact";
+import { SearchContact } from "../../components/SearchContact/SearchContact";
+import s from './ContactsBook.module.scss'
+
+const contacts = [
+	{ id: "1", name: "Александр", phoneNumber: "89960890043" },
+	{ id: "2", name: "Алексей", phoneNumber: "89211100043" },
+	{ id: "3", name: "Андрей", phoneNumber: "893260090043" },
+];
 
 export const ContactsBook = () => {
-  return (
-    <div>ContactsBook</div>
-  )
-}
+	return (
+		<div>
+			<CreateContact />
+			<SearchContact />
+      <div className={s.cards}>
+			{contacts.map(el => <ContactCard key={el.id} contact={el} />)}
+      </div>
+		</div>
+	);
+};
