08b713e2281877c4a09cf5f500ce5dbdd63cd860
markcoma <dex.go@yandex.ru>
2022-04-16 22:23:07 +0300

    contactsAPI

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -11,77 +11,25 @@ export const authAPI = {
 		});
 	},
 	registration(email, password) {
-		return ins.post("/auth/registration", { 
+		return ins.post("/auth/registration", {
 			email,
-			password });
+			password,
+		});
 	},
 };
-
-export const createContact = async (name, numberPhone) => {
-	try {
-		await axios
-			.post("http://localhost:5000/api/contacts", {
-				name,
-				numberPhone,
-			})
-			.then((res) => console.log(res));
-	} catch (error) {
-		console.log(error);
-	}
+export const contactsAPI = {
+	getContacts(userId) {
+		ins.get("/contscts", {
+			params: { userId },
+		});
+	},
+	createContact(name, numberPhone) {
+		ins.post("/contatcs", {
+			name,
+			numberPhone,
+		});
+	},
+	removeContacts(id) {
+		ins.delete(`/contacts:${id}`);
+	},
 };
-export const getContacts = async (userId) => {
-	try {
-		await axios
-			.get("http://localhost:5000/api/contacts", {
-				params: { userId },
-			})
-			.then((res) => console.log(res.data));
-	} catch (error) {
-		console.log(error);
-	}
-};
-export const removeContact = async (id) => {
-	try {
-		await axios
-			.post(`http://localhost:5000/api/contacts:${id}`)
-			.then(() => {
-				/* вызываем функцию получения списка контактов */
-			});
-	} catch (error) {
-		console.log(error);
-	}
-};
-// export const userAPI = {};
-
-// export const сontactsAPI = {
-// 	getContact() {
-// 		return ins.get(`/contact`);
-// 	},
-
-// 	pushContact(body: contact) {
-// 		return ins.post("/contact", body);
-// 	},
-
-// 	removeContact(id: string) {
-// 		return ins.delete(`/contact/${id}`);
-// 	},
-// };
-// export const authAPI = {
-// 	me() {
-// 		return ins.get(`auth/me`);
-// 	},
-// 	login(email: string, password: string) {
-// 		return ins.post(`auth/login`, { email, password });
-// 	},
-// 	logout() {
-// 		return ins.delete(`auth/login`);
-// 	},
-
-// 	registration(email: string, password: string) {
-// 		try {
-// 			return ins.post("/auth/registration", { email, password });
-// 		} catch (error) {
-// 			alert(error);
-// 		}
-// 	},
-// };
