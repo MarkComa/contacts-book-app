150526c22c05457a3d137b4dbe7ce5a2e80fecf2
markcoma <dex.go@yandex.ru>
2022-04-25 21:32:04 +0300

    react 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-ReactDOM.render(
-	<Provider store={store}>
+function StartApp() {
+	return (<Provider store={store}>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root"),
-);
+	</Provider>)
+};
+
+const container = document.getElementById("root")  as HTMLDivElement
+const root = ReactDOM.createRoot(container!)
+root.render(<StartApp />)
